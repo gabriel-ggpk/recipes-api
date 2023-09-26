04c447229515ebae04d730d56213809b5e4cabee
gabriel-ggpk <gabriel.cavalcanti.28@gmail.com>
2023-09-26 01:43:45 -0300

    adding db to dockerfile

---
+++ app/app/tests.py
@@ -0,0 +1,16 @@
+from django.test import SimpleTestCase
+
+from app import calc
+
+
+class CalcTest(SimpleTestCase):
+
+    def test_add_numbers(self):
+        res = calc.add(5, 6)
+
+        self.assertEqual(res, 11)
+
+    def test_sub_num(self):
+        res = calc.sub(10, 15)
+
+        self.assertEqual(res, 5)
